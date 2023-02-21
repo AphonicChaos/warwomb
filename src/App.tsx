7de746d7df9339a91b06f6b8002d6c6c4c11738d
Edwin Marshall <emarshall85@gmail.com>
2023-02-21 01:08:13 -0800

    Started playing with chakra

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,22 @@
-import { 
-  Stage, 
-  Layer, 
-} from 'react-konva';
-import Konva from 'konva';
-import { Ruler, useRuler } from './Ruler';
+import { ChakraProvider, Button, ColorModeScript, useColorMode } from '@chakra-ui/react';
+import { theme } from './theme';
+
+const Root = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  console.log(colorMode);
+  return (
+    <Button 
+      onClick={toggleColorMode}
+    >
+      Hi
+    </Button>
+  );
+};
 
 export const App = () => {
-  const ruler = useRuler();
-
-  const handleContextMenu = (e: Konva.KonvaEventObject<MouseEvent>) => {
-    e.evt.preventDefault();
-  };
-
   return (
-    <Stage 
-      width={window.innerWidth} 
-      height={window.innerHeight}
-      onContextMenu={handleContextMenu}
-      onMouseDown={ruler.down}
-      onMouseUp={ruler.up}
-      onMouseMove={ruler.move}
-    >
-      <Layer>
-        <Ruler
-          stroke="black"
-          points={ruler.points}
-          distance={ruler.distance}
-        />
-      </Layer>
-    </Stage>
+    <ChakraProvider theme={theme}>
+      <Root />
+    </ChakraProvider>
   );
-}
+};
