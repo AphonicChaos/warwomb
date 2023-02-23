96dfadc812820109303736220d71bce864d9ee99
Edwin Marshall <emarshall85@gmail.com>
2023-02-22 23:41:49 -0800

    Fixed a bunch of styling (#8)

--- src/App.tsx
+++ src/App.tsx
@@ -1,78 +1,52 @@
 import { 
-  Box,
   ChakraProvider,
-  Drawer,
-  DrawerBody,
-  DrawerCloseButton,
-  DrawerContent,
-  DrawerHeader,
-  DrawerOverlay,
   Flex,
-  IconButton,
-  Spacer,
-  useColorMode,
+  Grid,
+  GridItem,
   useDisclosure,
 } from '@chakra-ui/react';
 import { Table } from './Table';
 import { theme } from './theme';
-import { SunIcon, MoonIcon, HamburgerIcon } from '@chakra-ui/icons';
+import  { PageHeader } from './PageHeader';
+import { Toolbox } from './Toolbox';
+import { useMeasure } from '@react-hookz/web';
 
 const rockyGroundUrl = 'https://www.myfreetextures.com/wp-content/uploads/2012/05/2011-06-11-09606.jpg';
 
 const Root = () => {
-  const { colorMode, toggleColorMode } = useColorMode();
   const { 
     isOpen: toolboxIsOpen,
     onOpen: onToolboxOpen,
     onClose: onToolboxClose
   } = useDisclosure();
+  const [size, ref] = useMeasure(true);
 
   return (
-    // outer container
-    <Flex flexDirection="column" height="100%">
-      {/* top icons */}
-      <Flex>
-        <IconButton 
-          aria-label='Menu'
-          icon={<HamburgerIcon />}
-          onClick={onToolboxOpen}
+    <Grid
+      templateAreas={`"header"
+                      "main"
+                      "footer"`}
+      gridTemplateRows={'50px 1fr 50px'}
+      gridTemplateColumns={'1fr'}
+      h='100vh'
+    >
+      <GridItem area="header">
+        <PageHeader onToolboxOpen={onToolboxOpen} />
+      </GridItem>
+      {/* @ts-ignore */}
+      <GridItem area="main" ref={ref}>
+        <Toolbox isOpen={toolboxIsOpen} onClose={onToolboxClose} />
+        <Table 
+          backgroundUrl={rockyGroundUrl} 
+          size={size && size.height - 50} 
         />
-        <Spacer />
-        <IconButton 
-          aria-label='Color mode'
-          icon={
-            colorMode === 'dark'
-            ? <SunIcon />
-            : <MoonIcon />
-          }
-          onClick={toggleColorMode}
-        />
-      </Flex>
-      <Box>
-        <Drawer
-          isOpen={toolboxIsOpen}
-          placement="left"
-          onClose={onToolboxClose}
-        >
-          <DrawerOverlay />
-          <DrawerContent>
-            <DrawerCloseButton />
-            <DrawerHeader>
-            Tools
-            </DrawerHeader>
-            <DrawerBody>
-            <p>
-                Here is where all the various tools you might
-                need during a game would go.
-            </p>
-            </DrawerBody>
-          </DrawerContent>
-        </Drawer>
-        <Table width={800} height={600} backgroundUrl={rockyGroundUrl} />
-      </Box>
-      <Spacer />
-      {/* Hand */}
-    </Flex>
+      </GridItem>
+      <GridItem area="footer">
+        <Flex justifyContent="center">
+          Hand goes here
+        </Flex>
+      </GridItem>
+    </Grid>
   );
 };
 
